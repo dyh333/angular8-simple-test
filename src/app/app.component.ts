95a7dd0ca69578e15b01bb1a938adadc32d37b55
dingyihui <dingyihui@cmss.chinamobile.com>
2019-07-16 00:30:05 +0800

    [required] 50-character subject line
    Code Source From: Self Code
    
    Description:
    Jira:
    市场项目编号（名称）:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,93 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular8-simple-test';
+  mode = false;
+  dark = false;
+  menus = [
+    {
+      level: 1,
+      title: 'Mail Group',
+      icon: 'mail',
+      open: true,
+      selected: false,
+      disabled: false,
+      children: [
+        {
+          level: 2,
+          title: 'Group 1',
+          icon: 'bars',
+          open: false,
+          selected: false,
+          disabled: false,
+          children: [
+            {
+              level: 3,
+              title: 'Option 1',
+              selected: false,
+              disabled: false
+            },
+            {
+              level: 3,
+              title: 'Option 2',
+              selected: false,
+              disabled: true
+            }
+          ]
+        },
+        {
+          level: 2,
+          title: 'Group 2',
+          icon: 'bars',
+          selected: true,
+          disabled: false
+        },
+        {
+          level: 2,
+          title: 'Group 3',
+          icon: 'bars',
+          selected: false,
+          disabled: false
+        }
+      ]
+    },
+    {
+      level: 1,
+      title: 'Team Group',
+      icon: 'team',
+      open: false,
+      selected: false,
+      disabled: false,
+      children: [
+        {
+          level: 2,
+          title: 'User 1',
+          icon: 'user',
+          selected: false,
+          disabled: false
+        },
+        {
+          level: 2,
+          title: 'User 2',
+          icon: 'user',
+          selected: false,
+          disabled: false
+        }
+      ]
+    }
+  ];
+
+  status=0;
+
+
+  onSelectChange() {
+    this.menus[0].children[1].selected = false;
+    this.menus[0].children[2].selected = true;
+
+    console.log(this.menus);
+  }
+
+  onTimelineChange() {
+    // TODO: change dotTemplate to dotTemplate2
+    this.status = 1;
+  }
 }
