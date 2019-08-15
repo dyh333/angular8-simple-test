5c74b9c12cfcafd98814ea1c06442d9f7bb32184
dingyihui <dingyihui@cmss.chinamobile.com>
2019-08-15 14:21:21 +0800

    [required] 50-character subject line
    Code Source From: Self Code
    
    Description:
    Jira:
    市场项目编号（名称）:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularResizedEventModule } from 'angular-resize-event';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
-import zh from '@angular/common/locales/zh';
-
-registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -19,12 +11,9 @@ registerLocaleData(zh);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgZorroAntdModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    AngularResizedEventModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
