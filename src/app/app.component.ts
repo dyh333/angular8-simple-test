d6c37eb79f99b1d4112dfec22fedb4cbff87911b
dingyihui <dingyihui@cmss.chinamobile.com>
2019-10-12 14:30:20 +0800

    [required] 50-character subject line
    Code Source From: Self Code
    
    Description:
    Jira:
    市场项目编号（名称）:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
-import { ResizedEvent } from 'angular-resize-event';
+import { string2 } from 'dyh-ts-lib';
+// import * as dyh from 'dyh-ts-lib';
+// import { arrayTest, arrayEqual } from 'dyh-ts-lib';
+import { arrayTest, arrayEqual } from '@cmss/jslib';
+
+function uniqueArray<T>(array: Array<T>, uniqueField: keyof T): Array<T> {
+  const hash: { [key: string]: boolean } = {};
+
+  return array.reduce((items: Array<T>, next: T) => {
+    if (!hash[(next[uniqueField] as unknown) as string]) {
+      hash[(next[uniqueField] as unknown) as string] = true;
+
+      items = [...items, next];
+    }
+    return items;
+  }, []);
+}
+
 
 @Component({
   selector: 'app-root',
@@ -10,10 +27,8 @@ export class AppComponent {
   width: number;
   height: number;
 
-  onResized(event: ResizedEvent) {
-    this.width = event.newWidth;
-    this.height = event.newHeight;
-
-    console.log(this.width + ',' + this.height);
+  constructor() {
+    const a = [{id: 1, value: 1}, {id: 2, value: 2}, {id: 1, value: 3}];
+    console.log(uniqueArray(a, 'id'));
   }
 }
